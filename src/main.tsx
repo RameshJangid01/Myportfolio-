10a7d86a5ac6af593ead2a42283b3af32afeca76
RameshJangid <rameshnagal87@gmail.com>
2026-07-14 15:36:13 +0530

    Added Google Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
+import { createRoot } from "react-dom/client";
+import ReactGA from "react-ga4";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+// Google Analytics Initialize
+ReactGA.initialize("G-RWQV1ZL48N");
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+// First Page View
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
+
+createRoot(document.getElementById("root")!).render(<App />);
